1a23061bf3222a9e9b2fd4cdfea2267e78eb36e8
Jacques Wagener <6917456+jacqueswww@users.noreply.github.com>
2024-03-09 13:10:01 +0200

    Add chat history

--- app.py
+++ app.py
@@ -20,16 +20,17 @@ from langchain.prompts.chat import (
 from langchain_core.prompts.chat import (
     MessagesPlaceholder,
 )
-
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.tools import BaseTool, StructuredTool, tool
-from langchain.agents import AgentExecutor, create_openai_functions_agent, load_tools
-from langchain.agents import initialize_agent 
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain.tools import tool
+from langchain.agents import AgentExecutor
+from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+from langchain.agents.format_scratchpad.openai_tools import (
+    format_to_openai_tool_messages,
+)
 
 store = {}
+MEMORY_KEY = "chat_history"
+chat_history = []
 
 
 class MultiplySchema(BaseModel):
@@ -54,11 +55,6 @@ def get_routes():
     return requests.get('https://fa-api-prod.bluemarket.io//public_api/flights/routes/').json()
 
 
-# llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
-# llm_with_tools = llm.bind_tools([
-#     convert_to_openai_tool(Multiply()),
-#     convert_to_openai_tool(get_routes)
-# ])
 system_prompt = """You are a helpful assistant. Your name is Flybot. You are responsible for booking and managing FlySafair bookings.
 FlySafair is a low cost domestic airline based in Southern Africa.
 Flybot is talkative and provides lots of specific details from its context.
@@ -68,22 +64,32 @@ You only may assist with FlySafair, bookings & flight queries.
 
 prompt = ChatPromptTemplate.from_messages([
     SystemMessagePromptTemplate(prompt=PromptTemplate(input_variables=[], template=system_prompt)),
-    MessagesPlaceholder(variable_name='chat_history', optional=True),
+    MessagesPlaceholder(variable_name='chat_history'),
     HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['input'], template='{input}')),
     MessagesPlaceholder(variable_name='agent_scratchpad')
 ])
 
 
 def init_action():
-    llm = ChatOpenAI(model="gpt-4", temperature=0.1)
+    llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0.1)
+    llm_with_tools = llm.bind_tools([
+        convert_to_openai_tool(Multiply()),
+        convert_to_openai_tool(get_routes)
+    ])
     tools = [
         Multiply(),
         get_routes,
     ]
-    # from langchain import hub
-    # prompt2 = hub.pull("hwchase17/openai-functions-agent")
-    agent = create_openai_functions_agent(llm, tools, prompt)
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, return_intermediate_steps=True)
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_to_openai_tool_messages(
+                x["intermediate_steps"]
+            ),
+            "chat_history": lambda x: x["chat_history"],
+        } | prompt | llm_with_tools | OpenAIToolsAgentOutputParser()
+    )
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
     return agent_executor
 
 
@@ -92,8 +98,10 @@ while True:
     inp = input()
     if inp.lower() == 'exit':
         break
-
-    result = agent_executor.invoke({"input": inp})
-    # print(result.tool)
-    # print(result.tool_input)
-    print(result)
+    result = agent_executor.invoke({"input": inp, "chat_history": chat_history})
+    chat_history.extend(
+        [
+            HumanMessage(content=inp),
+            AIMessage(content=result["output"]),
+        ]
+    )
